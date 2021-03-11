651ac157699b8b46ffa1563066c466a34449570a
George Carder <georgercarder@gmail.com>
2021-03-11 14:18:11 -0800

    bytes not byte[]

--- contracts/MemArray.sol
+++ contracts/MemArray.sol
@@ -7,7 +7,7 @@ contract MemArray {
 	uint256 constant private wordSize = 32;
 	uint256 constant private dataSize = 32; // 32 `length`
 
-	function bytesToUint256(byte[] memory arr, uint256 from, uint256 to) private pure returns(uint256) {
+	function bytesToUint256(bytes memory arr, uint256 from, uint256 to) private pure returns(uint256) {
 		uint256 ret;
 		uint256 range = to - from;
 		for (uint256 i = from; i < to; i++) {
@@ -16,11 +16,11 @@ contract MemArray {
 		return ret;
 	}
 
- 	function getArrayLength(byte[] memory arr) private pure returns(uint256) {
+ 	function getArrayLength(bytes memory arr) private pure returns(uint256) {
 		return bytesToUint256(arr, 0, wordSize); // from, to
 	} 
 
-	function copy(byte[] memory dst, byte[] memory src) private pure returns(byte[] memory) {
+	function copy(bytes memory dst, bytes memory src) private pure returns(bytes memory) {
  		uint256 sizeSrc = src.length; // total allocated
 		for (uint256 i = 0; i < sizeSrc; i++) {
 			dst[i] = src[i];
@@ -35,11 +35,11 @@ contract MemArray {
 		return 2 * pow2(exp - 1);
 	}
 
-	function allocate(uint256 targetLen) private pure returns(byte[] memory){
-		return new byte[](targetLen);	
+	function allocate(uint256 targetLen) private pure returns(bytes memory){
+		return new bytes(targetLen);	
 	}
 
-	function Uint256IntoArr(byte[] memory arr, uint256 from, uint256 to, uint256 value) private pure returns(byte[] memory) {
+	function Uint256IntoArr(bytes memory arr, uint256 from, uint256 to, uint256 value) private pure returns(bytes memory) {
 		bytes32 bValue = bytes32(value);
 		uint256 range = to - from;
 		for (uint256 i = from; i < to; i++) {
@@ -48,11 +48,11 @@ contract MemArray {
 		return arr;
 	}
 
-	function setLength(byte[] memory arr, uint256 length) private pure returns(byte[] memory) {
+	function setLength(bytes memory arr, uint256 length) private pure returns(bytes memory) {
 		return Uint256IntoArr(arr, 0, wordSize, length); // from, to
 	}
 
-	function push(byte[] memory arr, byte value) internal returns(byte[] memory) {
+	function push(bytes memory arr, byte value) internal returns(bytes memory) {
  		uint256 sizeArr = arr.length; // total allocated
 		if (sizeArr < wordSize) {
 			arr = allocate(pow2(6)); 
@@ -60,7 +60,7 @@ contract MemArray {
 		}
 		uint256 lenArr = getArrayLength(arr);
 		if (lenArr >= (sizeArr - dataSize)) { // there is no room!
-			byte[] memory newArr = allocate(2*sizeArr);	
+			bytes memory newArr = allocate(2*sizeArr);	
 			newArr = copy(newArr, arr);
 			arr = newArr;
 		} // now there is room
@@ -72,17 +72,17 @@ contract MemArray {
   	}
 
 	// private unchecked
-	function _at(byte[] memory arr, uint256 idx) private pure returns(byte) {
+	function _at(bytes memory arr, uint256 idx) private pure returns(byte) {
 		return arr[idx+dataSize];	
 	}
 
 	// private unchecked
-	function _delete(byte[] memory arr, uint256 idx) private pure returns(byte[] memory) {
+	function _delete(bytes memory arr, uint256 idx) private pure returns(bytes memory) {
 		arr[idx+dataSize] = byte(0x00); // delete
 		return arr;
 	}
 
-	function pop(byte[] memory arr) internal pure returns(byte[] memory, byte, bool) {
+	function pop(bytes memory arr) internal pure returns(bytes memory, byte, bool) {
 		bool ok = true;
 		byte ret;
 		uint256 lenArr = getArrayLength(arr);
